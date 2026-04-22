15b41c150a1792b7b0bf2f9102b083c7232392cb
Ayaan <ayaanansari9997843139@gmail.com>
2026-04-22 09:23:49 +0530

    Update api.ts

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const baseAPI = axios.create({
   // baseURL: "http://localhost:5000",
-  baseURL: "https://ultron-backend-qoe2.onrender.com",
+  baseURL: "https://ultron-backend-ssoc.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
